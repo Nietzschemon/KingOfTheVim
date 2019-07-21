c913adc7080a8aec76ff5d42a70678dca22c2f23
Stefan Ekblom <stefan.ekblom@hotmail.com>
2019-07-21 15:39:05 +0200

    Added class Size to store dito

---
+++ core/src/com/kingofthevim/game/basicvim/Size.java
@@ -0,0 +1,22 @@
+package com.kingofthevim.game.basicvim;
+
+public class Size {
+
+
+    private int width = 22;
+    private int height = 44;
+
+    public Size(int width, int height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+}
