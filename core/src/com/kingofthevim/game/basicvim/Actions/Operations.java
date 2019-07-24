0b1d6f1c8464fd56d73a4874d7379a7d57fc13de
Stefan Ekblom <stefan.ekblom@hotmail.com>
2019-07-24 12:58:14 +0200

    Added overloaded method for deleteChar

--- core/src/com/kingofthevim/game/basicvim/Actions/Operations.java
+++ core/src/com/kingofthevim/game/basicvim/Actions/Operations.java
@@ -8,11 +8,25 @@ import java.util.ArrayList;
 public class Operations extends Movement{
 
 
+    /**
+     * "Deletes" the current occupied cell of the VimObject.
+     *
+     * This is done by movining all the cell data one cell
+     * to the left and clearing the last cell in the row.
+     * @param vimObj the object who's cell is to be "deleted"
+     */
     protected void deleteChar(VimObject vimObj){
 
+        int position = vimObj.getPosition().getCurrColumn();
+
+        deleteChar(vimObj, position);
+    }
+
+    protected void deleteChar(VimObject vimObj, int position){
         ArrayList<Cell> cellList = vimObj.getVimMatrix().getCellMatrix().get(vimObj.getPosition().getCurrRow());
 
-        for (int i = vimObj.getPosition().getCurrColumn(); i < cellList.size() - 1; i++) {
+
+        for (int i = position; i < cellList.size() - 1; i++) {
 
             cellList.get(i).setCellProperties(cellList.get( i + 1 ).getCellProperties());
         }
