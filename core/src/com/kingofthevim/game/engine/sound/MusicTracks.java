d1a12c33d234d1943a641f6e45b997b4fc3dd97b
Stefan Ekblom <stefan.ekblom@hotmail.com>
2019-08-18 14:09:18 +0200

    Added music enums

--- core/src/com/kingofthevim/game/engine/sound/MusicTracks.java
+++ core/src/com/kingofthevim/game/engine/sound/MusicTracks.java
@@ -2,8 +2,9 @@ package com.kingofthevim.game.engine.sound;
 
 public enum MusicTracks {
     BUNNY("sound/music/laborintMusic/bunny.wav"),
-    TRUMPET("sound/music/trumpet.wav");
-    //lab();
+    TRUMPET("sound/music/trumpet.wav"),
+    YASHA("sound/music/labMusic1/labMusic1pcm.wav"),
+    EROL("sound/music/labMusic2pcm.wav");
 
     private final String trackPath;
 
